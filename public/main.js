37ce67c2d514d1ad3cf01d0fea10683690df2382
pedro88 <sandrep_85@hotmail.com>
2024-11-11 16:15:05 +0100

    added js index page and listener to oui and non button

--- public/main.js
+++ public/main.js
@@ -1,89 +1,83 @@
 // public/main.js
 const socket = io();
 
+//Création de la page principal avec les logos
 const mainPage = () => {
+  const indexDiv = document.createElement("div");
+  indexDiv.className = "index";
+  indexDiv.id = "index";
 
-const indexDiv = document.createElement("div");
-indexDiv.className = "index";
-indexDiv.id = "index";
+  const titleParagraph = document.createElement("p");
+  titleParagraph.innerHTML = "Et toi,<br> quel réseau social utilises-tu?";
+  indexDiv.appendChild(titleParagraph);
 
+  const logoContainer = document.createElement("div");
+  logoContainer.className = "logoContainer";
+  logoContainer.id = "logo-selection";
+  indexDiv.appendChild(logoContainer);
 
-const titleParagraph = document.createElement("p");
-titleParagraph.innerHTML = "Et toi,<br> quel réseau social utilises-tu?";
-indexDiv.appendChild(titleParagraph);
+  const logos = [
+    { name: "facebook", src: "./logo/facebook.svg", alt: "Facebook" },
+    { name: "insta", src: "./logo/insta.svg", alt: "Insta" },
+    { name: "tiktok", src: "./logo/tiktok.svg", alt: "Tiktok" },
+    { name: "bereal", src: "./logo/bereal.svg", alt: "Bereal" },
+    { name: "steam", src: "./logo/steam.svg", alt: "Steam" },
+    { name: "aliexpress", src: "./logo/aliexpress.svg", alt: "Aliexpress" },
+    { name: "snapchat", src: "./logo/snapchat.svg", alt: "Snapchat" },
+  ];
 
+  logos.forEach((logo) => {
+    const button = document.createElement("button");
+    button.className = "logo";
+    button.setAttribute("data-logo", logo.name);
 
-const logoContainer = document.createElement("div");
-logoContainer.className = "logoContainer";
-logoContainer.id = "logo-selection";
-indexDiv.appendChild(logoContainer);
+    const img = document.createElement("img");
+    img.src = logo.src;
+    img.alt = logo.alt;
+    img.className = "logo";
 
+    button.appendChild(img);
+    logoContainer.appendChild(button);
+  });
 
-const logos = [
-  { name: "facebook", src: "./logo/facebook.svg", alt: "Facebook" },
-  { name: "insta", src: "./logo/insta.svg", alt: "Insta" },
-  { name: "tiktok", src: "./logo/tiktok.svg", alt: "Tiktok" },
-  { name: "bereal", src: "./logo/bereal.svg", alt: "Bereal" },
-  { name: "steam", src: "./logo/steam.svg", alt: "Steam" },
-  { name: "aliexpress", src: "./logo/aliexpress.svg", alt: "Aliexpress" },
-  { name: "snapchat", src: "./logo/snapchat.svg", alt: "Snapchat" },
-];
+  document.body.appendChild(indexDiv);
+};
 
-
-logos.forEach(logo => {
-
-  const button = document.createElement("button");
-  button.className = "logo";
-  button.setAttribute("data-logo", logo.name);
-
-
-  const img = document.createElement("img");
-  img.src = logo.src;
-  img.alt = logo.alt;
-  img.className = "logo";
-
-
-  button.appendChild(img);
-  logoContainer.appendChild(button);
-});
-
-
-document.body.appendChild(indexDiv);  
-}
-
-
-
-document.querySelectorAll(".logo").forEach(button => {
+//Création de l'écoute des différents éléments
+const addListener = () => {
+  document.querySelectorAll(".logo").forEach((button) => {
     button.addEventListener("click", () => {
-        const logoId = button.getAttribute("data-logo");
-        socket.emit("choixLogo", logoId);
+      const logoId = button.getAttribute("data-logo");
+      socket.emit("choixLogo", logoId);
 
-        const element = document.getElementById("index");
-        element.remove()
+      const element = document.getElementById("index");
+      if (element) element.remove();
+      console.log(logoId);
 
-        const ouiButton = document.createElement("button");
-        ouiButton.textContent = "Oui";
-        ouiButton.id = "oui-btn";
-        document.body.appendChild(ouiButton);
-        ouiButton.addEventListener("click", () => {
-            alert("OUI!")
-        });
+      const ouiButton = document.createElement("button");
+      ouiButton.textContent = "Oui";
+      ouiButton.id = "oui-btn";
+      document.body.appendChild(ouiButton);
+      ouiButton.addEventListener("click", () => {
+        socket.emit("choice", logoId);
+        ouiButton.remove();
+        nonButton.remove();
+      });
 
-        const nonButton = document.createElement("button");
-        nonButton.textContent = "Non";
-        nonButton.id = "non-btn";
-        document.body.appendChild(nonButton);
-        nonButton.addEventListener("click", () => {
-            alert("NON!")
-        });
-
+      const nonButton = document.createElement("button");
+      nonButton.textContent = "Non";
+      nonButton.id = "non-btn";
+      document.body.appendChild(nonButton);
+      nonButton.addEventListener("click", () => {
+        socket.emit("choice", logoId);
+        ouiButton.remove();
+        nonButton.remove();
+      });
 
     });
-});
-
-
-
-// Réinitialisation de l'application
-document.getElementById("reset-btn").addEventListener("click", () => {
+  });
+  // Réinitialisation de l'application
+  document.getElementById("reset-btn").addEventListener("click", () => {
     socket.emit("reset");
-});
+  });
+};
--- server.js
+++ server.js
@@ -1,55 +1,67 @@
 // server.js
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 // Servir les fichiers statiques (HTML, CSS, JS)
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Écoute des connexions Socket.IO
-io.on('connection', (socket) => {
-    console.log('Nouvelle connexion établie:', socket.id);
+io.on("connection", (socket) => {
+  console.log("Nouvelle connexion établie:", socket.id);
 
-    // Réception de l'événement de sélection de logo
-    socket.on('choixLogo', (logoId) => {
-        console.log(`Logo choisi: ${logoId}`);
-        if(logoId === 'facebook'){
-            io.emit('miseAJourEcrans', `vous etes bien sur le logo facebook!`);
-        }
-        else if(logoId === 'insta'){
-            io.emit('miseAJourEcrans', `vous etes bien sur le logo instagram!`);
-        }
-        else if(logoId === 'tiktok'){
-            io.emit('miseAJourEcrans', `vous etes bien sur le logo tiktok!`);
-        }
-        else if(logoId === 'bereal'){
-            io.emit('miseAJourEcrans', `vous etes bien sur le logo bereal!`);
-        }
-        else if(logoId === 'steam'){
-            io.emit('miseAJourEcrans', `vous etes bien sur le logo steam!`);
-        }
-        else if(logoId === 'aliexpress'){
-            io.emit('miseAJourEcrans', `vous etes bien sur le logo aliexpress!`);
-        }
-        else if(logoId === 'snapchat'){
-            io.emit('miseAJourEcrans', `vous etes bien sur le logo snapchat!`);
-        }
-        else {
-            io.emit('miseAJourEcrans', `vous etes bien perdu!`);
-        };  
+  // Réception de l'événement de sélection de logo
+  socket.on("choixLogo", (logoId) => {
+    console.log(`Logo choisi: ${logoId}`);
+    if (logoId === "facebook") {
+      io.emit("miseAJourEcrans", `vous etes bien sur le logo facebook!`);
+    } else if (logoId === "insta") {
+      io.emit("miseAJourEcrans", `vous etes bien sur le logo instagram!`);
+    } else if (logoId === "tiktok") {
+      io.emit("miseAJourEcrans", `vous etes bien sur le logo tiktok!`);
+    } else if (logoId === "bereal") {
+      io.emit("miseAJourEcrans", `vous etes bien sur le logo bereal!`);
+    } else if (logoId === "steam") {
+      io.emit("miseAJourEcrans", `vous etes bien sur le logo steam!`);
+    } else if (logoId === "aliexpress") {
+      io.emit("miseAJourEcrans", `vous etes bien sur le logo aliexpress!`);
+    } else if (logoId === "snapchat") {
+      io.emit("miseAJourEcrans", `vous etes bien sur le logo snapchat!`);
+    } else {
+      io.emit("miseAJourEcrans", `vous etes bien perdu!`);
+    }
+  });
 
+  //Réception de l'évenement du choix OUI/NON
+  socket.on("choice", (logoId) => {
+    if (logoId === "facebook") {
+      console.log("Merci d'avoir choisi le logo Facebook");
+      io.emit("miseAJourEcrans", `Facebook blablabla`);
+    } else if (logoId === "insta") {
+      io.emit("miseAJourEcrans", `Insta blablabla`);
+    } else if (logoId === "snapchat") {
+      io.emit("miseAJourEcrans", `Snapchat blablabla`);
+    } else if (logoId === "steam") {
+      io.emit("miseAJourEcrans", `Steam blablabla`);
+    } else if (logoId === "tiktok") {
+      io.emit("miseAJourEcrans", `Tiktok blablabla`);
+    } else if (logoId === "bereal") {
+      io.emit("miseAJourEcrans", `Bereal blablabla`);
+    } else if (logoId === "aliexpress") {
+      io.emit("miseAJourEcrans", `Aliexpress blablabla`);
+    } else {
+      io.emit("miseAJourEcrans", `Toujours perdu...`);
+    }
+  });
 
-        //io.emit('miseAJourEcrans', `Merci d'avoir choisi le logo ${logoId}`);
-    });
-
-    // Réinitialisation des écrans
-    socket.on('reset', () => {
-        io.emit('resetEcrans');
-    });
+  // Réinitialisation des écrans
+  socket.on("reset", () => {
+    io.emit("resetEcrans");
+  });
 });
 
 const PORT = 3000;
